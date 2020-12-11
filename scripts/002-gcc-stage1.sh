45dbaca50ddef7f53f6fa0f0dd6f4b986c485551
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2020-12-11 16:00:12 +0100

    Add missing stages

---
+++ scripts/002-gcc-stage1.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# 002-gcc-stage1.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
+
+## Download the source code.
+REPO_URL="https://gitlab.com/ps2max/toolchain/gcc.git"
+REPO_FOLDER="gcc"
+BRANCH_NAME="ee-toolchain-gcc9"
+if test ! -d "$REPO_FOLDER"; then
+	git clone -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+else
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+fi
+
+TARGET_ALIAS="ee" 
+TARGET="mips64r5900el-ps2-elf"
+
+OSVER=$(uname)
+## Apple needs to pretend to be linux
+if [ ${OSVER:0:6} == Darwin ]; then
+	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu"
+else
+	TARG_XTRA_OPTS=""
+fi
+
+## Determine the maximum number of processes that Make can work with.
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
+
+## Create and enter the toolchain/build directory
+mkdir build-$TARGET-stage1 && cd build-$TARGET-stage1 || { exit 1; }
+
+## Configure the build.
+../configure \
+  --quiet \
+  --prefix="$PS2DEV/$TARGET_ALIAS" \
+  --target="$TARGET" \
+  --enable-languages="c" \
+  --with-float=hard \
+  --with-newlib \
+  --disable-nls \
+  --disable-shared \
+  --disable-libssp \
+  --disable-libmudflap \
+  --disable-threads \
+  --disable-libgomp \
+  --disable-libquadmath \
+  --disable-target-libiberty \
+  --disable-target-zlib \
+  --without-ppl \
+  --without-cloog \
+  --with-headers=no \
+  --disable-libada \
+  --disable-libatomic \
+  --disable-multilib \
+  $TARG_XTRA_OPTS || { exit 1; }
+
+## Compile and install.
+make --quiet -j $PROC_NR clean   || { exit 1; }
+make --quiet -j $PROC_NR all     || { exit 1; }
+make --quiet -j $PROC_NR install || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
---
+++ scripts/003-newlib.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 003-newlib.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
+
+## Download the source code.
+REPO_URL="https://gitlab.com/ps2max/toolchain/newlib.git"
+REPO_FOLDER="newlib"
+BRANCH_NAME="ee-toolchain-gcc9"
+if test ! -d "$REPO_FOLDER"; then
+	git clone -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+else
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+fi
+
+TARGET_ALIAS="ee" 
+TARGET="mips64r5900el-ps2-elf"
+
+## Determine the maximum number of processes that Make can work with.
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
+
+## Create and enter the toolchain/build directory
+mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
+
+## Configure the build.
+CFLAGS_FOR_TARGET="-G0" ../configure --prefix="$PS2DEV/$TARGET_ALIAS" --target="$TARGET" || { exit 1; }
+
+## Compile and install.
+make clean && make -j $PROC_NR && make install && make clean || { exit 1; }
